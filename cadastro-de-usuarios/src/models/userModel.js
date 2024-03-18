7ca11d8d9468c837cd4491cd874a99981668b670
MAYCON PACHECO <151109977+mayconpachecooficial@users.noreply.github.com>
2024-03-18 15:25:56 -0400

    feat:
    Este código define a função createUser para inserir um novo usuário no banco de dados, utilizando parâmetros como nome, sobrenome, e-mail, hash da senha, documento e ID afiliado, e automaticamente define o usuário com o papel 'Member'. Retorna o ID gerado para o novo usuário.

--- cadastro-de-usuarios/src/models/userModel.js
+++ cadastro-de-usuarios/src/models/userModel.js
@@ -1,3 +1,7 @@
+// Este código define a função createUser para inserir um novo usuário no banco de dados, 
+// utilizando parâmetros como nome, sobrenome, e-mail, hash da senha, documento e ID afiliado, 
+// e automaticamente define o usuário com o papel 'Member'. Retorna o ID gerado para o novo usuário.
+
 const db = require('../utils/database');
 
 const createUser = async (user) => {
